934325ee9365550b883a1fc81a8693eaced8e203
Merite15 <86039889+Merite15@users.noreply.github.com>
2023-07-10 09:52:14 +0100

    remove unused files

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client'
+import { App } from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = document.querySelector('#root')
+
+createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
