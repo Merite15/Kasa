4b88a4ba3deaa49bf694484a077102146e322328
Merite15 <86039889+Merite15@users.noreply.github.com>
2023-07-10 14:16:23 +0100

    STEP 7 : Carousel de la page de logement

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/prop-types */
+
+import { useState } from 'react';
+
+export const Carousel = (props) => {
+    const { logement } = props
+
+    const [currentElement, setCurrentElement] = useState(0);
+
+    const handlePreviousClick = () => {
+        if (currentElement > 0) {
+            setCurrentElement(currentElement - 1);
+        }
+    };
+
+    const handleNextClick = () => {
+        if (currentElement < logement.pictures.length - 1) {
+            setCurrentElement(currentElement + 1);
+        }
+    };
+
+    return (
+        <div>
+            {logement.pictures && (
+                <div
+                    className="slide"
+                    style={{ backgroundImage: `url(${logement.pictures[currentElement]})` }}
+                >
+                    {logement.pictures.length > 1 && (
+                        <div className="slide-arrow">
+                            <div>
+                                {currentElement > 0 && (
+                                    <i onClick={handlePreviousClick} className="fa-solid fa-chevron-left fa-4x arrow" />
+                                )}
+                            </div>
+                            
+                            <div>
+                                {currentElement < logement.pictures.length - 1 && (
+                                    <i onClick={handleNextClick} className="fa-solid fa-chevron-right fa-4x arrow" />
+                                )}
+                            </div>
+                        </div>
+                    )}
+                    {logement.pictures.length > 1 && (
+                        <div className="count">{`${currentElement + 1}/${logement.pictures.length}`}</div>
+                    )}
+                </div>
+            )}
+        </div>
+    );
+};
--- src/pages/Card/Index.jsx
+++ src/pages/Card/Index.jsx
@@ -1,9 +1,28 @@
+import { Main } from "@/layouts/Main";
+import { Carousel } from "@/components/Carousel";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 export const Card = () => {
+
+    const params = useParams()
+
+    const [logement, setLogement] = useState({})
+
+    useEffect(() => {
+        fetch(`http://127.0.0.1:5173/logements.json`)
+            .then((response) => response.json()
+                .then((res) => {
+                    const data = res.filter(logement => logement.id === params.id)
+                    setLogement(data[0])
+                })
+                .catch((error) => console.log(error))
+            )
+    }, [params.id])
     return (
-        <>
-            <div>
-                <p> Details</p>
-            </div>
-        </>
-    )
+        <Main>
+            <Carousel
+                logement={logement}></Carousel>
+        </Main>
+    );
 }
