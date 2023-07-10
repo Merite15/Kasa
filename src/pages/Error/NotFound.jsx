b01f321d4ad3d7281e2eed3a7c3a19ec51e91f77
Merite15 <86039889+Merite15@users.noreply.github.com>
2023-07-10 11:40:02 +0100

    Step 6 : Page d'erreur

--- src/pages/Error/NotFound.jsx
+++ src/pages/Error/NotFound.jsx
@@ -1,9 +1,17 @@
+import { Main } from "@/layouts/Main";
+import { NavLink } from "react-router-dom";
+
 export const NotFound = () => {
     return (
-        <>
-            <div>
-                <p> Erreur</p>
+        <Main>
+            <div className="notFound">
+                <div>
+                    <div className="notFound-code">404</div>
+                    <div className="text-error">Oups! La page que vous demandez n&apos;existe pas.</div> 
+                    <div className="home-link"><NavLink className="home-nav-link" to="/">Retourner sur la page
+                        d’accueil</NavLink></div>
+                </div>
             </div>
-        </>
-    )
+        </Main>
+    );
 }
